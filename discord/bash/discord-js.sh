559116c41e5e7152d7a3c811ba366ad8c34c2362
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-27 19:22:06 +0700

    sorry sala

--- discord/bash/discord-js.sh
+++ discord/bash/discord-js.sh
@@ -5,6 +5,6 @@ if [[ ! -z ${UNINSTALL_PACKAGES} ]]; then
 /usr/local/bin/npm uninstall ${UNINSTALL_PACKAGES}; fi;
 if [ -f /home/container/package.json ]; 
 then  /usr/local/bin/npm install --production; fi;
-if [ -f ${RUN_BUILD} ];
+if [[ ! -z ${RUN_BUILD} ]];
 then /usr/local/bin/npm run build --if-present; 
 fi; /usr/local/bin/node /home/container/{{BOT_JS_FILE}}
