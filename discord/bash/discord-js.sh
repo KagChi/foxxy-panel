d04751f0192a40c76096fa8900bb24a2710b059e
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-27 19:29:22 +0700

    Update discord-js.sh

--- discord/bash/discord-js.sh
+++ discord/bash/discord-js.sh
@@ -1,10 +1,10 @@
 if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == "1" ]]; 
 then git pull; fi; 
-if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install {{NODE_PACKAGES}}; fi; 
+if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; 
 if [[ ! -z ${UNINSTALL_PACKAGES} ]]; then
 /usr/local/bin/npm uninstall ${UNINSTALL_PACKAGES}; fi;
 if [ -f /home/container/package.json ]; 
 then  /usr/local/bin/npm install --production; fi;
 if [[ ! -z ${RUN_BUILD} ]];
 then /usr/local/bin/npm run build --if-present; 
-fi; /usr/local/bin/node /home/container/{{BOT_JS_FILE}}
+fi; /usr/local/bin/node /home/container/${BOT_JS_FILE}
